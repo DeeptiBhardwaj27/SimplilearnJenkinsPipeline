35d1808c855907ec0d6c26d59155dbc3323c08b3
Deepti <deeptibhardwaj27@gmail.com>
2020-12-20 19:01:54 +0530

    1st commit

---
+++ src/main/java/com/example/SpringBootProjectSimplilearn/SpringBootProjectSimplilearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootProjectSimplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectSimplilearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectSimplilearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootProjectSimplilearn/SpringBootProjectSimplilearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootProjectSimplilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectSimplilearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
